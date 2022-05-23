f528cea9d32e22596e4006e20135c3910679c33a
Olle Ekberg <olleekberg88@gmail.com>
2022-05-23 10:29:41 +0200

    fixing calendar

--- Sources/SwiftCharts/LineChart/LineChart+View.swift
+++ Sources/SwiftCharts/LineChart/LineChart+View.swift
@@ -214,11 +214,10 @@ private extension SwiftCharts.LineChart.ChartView {
             .stroke(lineWidth: indicatorStrokeWidth)
             .fill(config.lineColor)
             .frame(width: indicatorCircleDiameter, height: indicatorCircleDiameter)
-            
             .overlay(
                 Circle()
                     .frame(width: indicatorCircleCenterDiameter, height: indicatorCircleCenterDiameter)
-                    .background(.white)
+                    .background(Color.white)
                     .opacity(0.3)
             )
             .offset(y: indicatorRadius)
